852ee5c88929069475b315f5f4094ce9d3531d2f
Matthes <matthes@leuffen.de>
2020-05-01 01:22:34 +0200

    demo!

--- src/Cli/CloudiaCli.php
+++ src/Cli/CloudiaCli.php
@@ -6,6 +6,7 @@ namespace Cloudia\Cli;
 
 use Phore\CliTools\Helper\GetOptResult;
 use Phore\CliTools\PhoreAbstractCli;
+use Phore\FileSystem\PhoreTempFile;
 
 class CloudiaCli extends PhoreAbstractCli
 {
@@ -34,8 +35,21 @@ class CloudiaCli extends PhoreAbstractCli
             "create_keypair" => function (array $argv) {
                 $this->out("Hello world", print_r($argv, true));
             },
+
             "say_hello" => function (array $argv) {
                 $this->out("Hello!" . PHP_EOL);
+            },
+
+            // open the default editor to enter some secret
+            "open_editor" => function(array $argv) {
+                $tmpFile = new PhoreTempFile();
+
+                // Open the editor
+                passthru("editor $tmpFile");
+
+                $this->out("You wrote:" . $tmpFile->get_contents());
+
+                // Temp file should be deleted on __destuct()
             }
         ]);
     }
